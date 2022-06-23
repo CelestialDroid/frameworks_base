664879c3634800e48b7a3126e1cfd7da76a65dfc
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-24 02:38:49 +0300

    SystemUI: screenshot: open long screenshot activity for partial screenshots
    The partial screenshot UI is basically unusable. Replace it
    with the long  screenshot UI which offers cropping, saving,
    canceling, and sharing capabilities.
    
    [Stallix] Adapt for A13 QPR3
    [imapotatook] Adapt for A14 release 1
    
    Change-Id: I27f7a811838f7e3f2057f567c7a350255996a8cb
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/TakeScreenshotService.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/TakeScreenshotService.java
@@ -37,6 +37,9 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.Insets;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
@@ -47,6 +50,7 @@ import android.os.RemoteException;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.util.Log;
+import android.view.WindowManager;
 import android.view.Display;
 import android.widget.Toast;
 
@@ -245,19 +249,10 @@ public class TakeScreenshotService extends Service {
         }
 
         Log.d(TAG, "Processing screenshot data");
-
-
-        if (mFeatureFlags.isEnabled(MULTI_DISPLAY_SCREENSHOT)) {
-            mTakeScreenshotExecutor.get().executeScreenshotsAsync(request, onSaved, callback);
-            return;
-        }
-        // TODO(b/295143676): Delete the following after the flag is released.
+        ScreenshotData screenshotData = ScreenshotData.fromRequest(request);
         try {
-            ScreenshotData screenshotData = ScreenshotData.fromRequest(
-                    request, Display.DEFAULT_DISPLAY);
-            mProcessor.processAsync(screenshotData, (data) ->
-                    dispatchToController(data, onSaved, callback));
-
+            mProcessor.processAsync(screenshotData,
+                    (data) -> dispatchToController(data, onSaved, callback));
         } catch (IllegalStateException e) {
             Log.e(TAG, "Failed to process screenshot request!", e);
             logFailedRequest(request);
@@ -267,13 +262,42 @@ public class TakeScreenshotService extends Service {
         }
     }
 
-    // TODO(b/295143676): Delete this.
-    private void dispatchToController(ScreenshotData screenshot,
+    private void dispatchToController(ScreenshotRequest request,
             Consumer<Uri> uriConsumer, RequestCallback callback) {
-        mUiEventLogger.log(ScreenshotEvent.getScreenshotSource(screenshot.getSource()), 0,
-                screenshot.getPackageNameString());
-        Log.d(TAG, "Screenshot request: " + screenshot);
-        mScreenshot.handleScreenshot(screenshot, uriConsumer, callback);
+        ComponentName topComponent = request.getTopComponent();
+        String packageName = topComponent == null ? "" : topComponent.getPackageName();
+        mUiEventLogger.log(
+                ScreenshotEvent.getScreenshotSource(request.getSource()), 0, packageName);
+
+        switch (request.getType()) {
+            case WindowManager.TAKE_SCREENSHOT_FULLSCREEN:
+                if (DEBUG_SERVICE) {
+                    Log.d(TAG, "handleMessage: TAKE_SCREENSHOT_FULLSCREEN");
+                }
+                mScreenshot.takeScreenshotFullscreen(topComponent, uriConsumer, callback);
+                break;
+            case WindowManager.TAKE_SCREENSHOT_SELECTED_REGION:
+                if (DEBUG_SERVICE) {
+                    Log.d(TAG, "handleMessage: TAKE_SCREENSHOT_SELECTED_REGION");
+                }
+                mScreenshot.takeScreenshotPartial(topComponent, uriConsumer, callback);
+                break;
+            case WindowManager.TAKE_SCREENSHOT_PROVIDED_IMAGE:
+                if (DEBUG_SERVICE) {
+                    Log.d(TAG, "handleMessage: TAKE_SCREENSHOT_PROVIDED_IMAGE");
+                }
+                Bitmap screenshot = request.getBitmap();
+                Rect screenBounds = request.getBoundsInScreen();
+                Insets insets = request.getInsets();
+                int taskId = request.getTaskId();
+                int userId = request.getUserId();
+
+                mScreenshot.handleImageAsScreenshot(screenshot, screenBounds, insets,
+                        taskId, userId, topComponent, uriConsumer, callback);
+                break;
+            default:
+                Log.wtf(TAG, "Invalid screenshot option: " + request.getType());
+        }
     }
 
     private void logFailedRequest(ScreenshotRequest request) {
