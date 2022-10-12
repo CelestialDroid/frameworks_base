dff08c4bf8143c902e111a6e1d7ed8a671401190
Anay Wadhera <anay1018@gmail.com>
2022-10-12 22:08:45 +0900

    SystemUI: Mark colorBackgroundDrawable as protected
    Change-Id: I39db23cc358fc38c31773ef2110c0cfbfb0569a6

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -135,9 +135,9 @@ open class QSTileViewImpl @JvmOverloads constructor(
     protected var showRippleEffect = true
 
     private lateinit var ripple: RippleDrawable
-    private lateinit var backgroundDrawable: LayerDrawable
-    private lateinit var backgroundBaseDrawable: Drawable
-    private lateinit var backgroundOverlayDrawable: Drawable
+    protected lateinit var backgroundDrawable: LayerDrawable
+    protected lateinit var backgroundBaseDrawable: Drawable
+    protected lateinit var backgroundOverlayDrawable: Drawable
 
     private var backgroundColor: Int = 0
     private var backgroundOverlayColor: Int = 0
